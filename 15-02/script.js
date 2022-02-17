285f64a1f64f966c1a2775e08a8858694c33266e
adilet <firminbaev2020@gmail.com>
2022-02-17 13:21:30 +0600

    init

---
+++ 15-02/script.js
@@ -0,0 +1,4 @@
+let name1 = 'adilet';
+let name2 = 'samarbekov';
+name1 = name2;
+alert(name2);
---
+++ 16-02/script.js
@@ -0,0 +1,14 @@
+let camelCase = ('0');
+let snake_case = ('2');
+let PascalCase = ('3');
+
+alert(camelCase);
+alert(snake_case);
+alert(PascalCase);
+
+
+let str1 = ('САЛАМ');
+let str2 = ('БИШКЕК');
+alert(`${str1},${str2}`);
+
+
