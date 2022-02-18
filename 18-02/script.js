bba0e7dbb8192b307edebc0d5a0af42c908734af
adilet <firminbaev2020@gmail.com>
2022-02-18 13:04:00 +0600

    feat: end tasks

---
+++ 18-02/script.js
@@ -0,0 +1,50 @@
+// taks#1
+let num = 10;
+num++;
+console.log(num);
+
+let num1 = 10;
+++num1;
+console.log(num1);
+
+let num2 = 10;
+num2--;
+console.log(num2);
+
+// taks#2
+let name = 'Adilet';
+console.log('Привет, ' + name + '!');
+
+// taks#3
+let age = 27;
+console.log('Мне ' + age + ' лет');
+
+// taks#4
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+// taks#5
+let num3 = 47;
+num3 += 7;
+alert(num3);
+console.log(num3);
+let num4 = 47;
+num4 -= 18;
+alert(num4);
+console.log(num4);
+let num5 = 47;
+num5 *= 10;
+alert(num5);
+console.log(num5);
+let num6 = 47;
+num6 /= 15;
+alert(num6);
+console.log(num6);
+
+
+
+
