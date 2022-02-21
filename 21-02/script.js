6e01a166ddb831a91621f32d0758d8179507ae1d
adilet <firminbaev2020@gmail.com>
2022-02-21 11:46:14 +0600

    fear: finish 21-02

---
+++ 21-02/script.js
@@ -0,0 +1,38 @@
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num ++;
+num --;
+alert(num);
+
+
+var num4 = 10;
+num4++
+++num4;
+--num4;
+alert(num4);
+
+
+let num1 = 22;
+let num2 = 44;
+let result = num1 == 22 && num2 == 88;
+let result2 = num1 > 55 && num2 == 44;
+let result3 = num1 < 40 && num2 >40;
+let result4 = num1 !=22 && num2 > 50;
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+
+let age = 99;
+let result5 = age > 12 || age == 100;
+let result6 = age != 99 || age <100;
+let result7 = age >= 112 || age <=88;
+let result8 = age != 100 || age > 77;
+console.log(result5);
+console.log(result6);
+console.log(result7);
+console.log(result8);
+
